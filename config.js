b0e1ad8bcc5077a928b35d6105e033f6d64c3f16
Vitor Baptista <vitor@vitorbaptista.com>
2017-08-04 12:56:02 +0100

    Fix links to *os-viewer*
    We weren't adding the slash, ending up with URLs like
    `https://staging.openspending.orgviewer/foobar`. This patch fixes that and
    validates the configured base URLs when building the project.

--- config.js
+++ config.js
@@ -1,10 +1,23 @@
 var baseUrl = process.env.OS_BASE_URL || 'https://staging.openspending.org';
 var osExplorerSearchHost = process.env.OS_EXPLORER_SEARCH_HOST || baseUrl;
+var searchUrl = process.env.OS_SEARCH_URL || osExplorerSearchHost + '/search/package';
+var authUrl = process.env.OS_EXPLORER_AUTH_HOST || baseUrl;
+var url = require('url');
+var assert = require('assert');
+
+function validateUrl(_url) {
+  var parsedUrl = url.parse(_url);
+
+  assert(parsedUrl.protocol, 'You need to define the URL protocol (' + _url + ')');
+  assert(!_url.endsWith('/'), 'The URL should not end with a slash (' + _url + ')');
+
+  return _url;
+}
 
 module.exports = {
-  baseUrl: baseUrl,
-  searchUrl: process.env.OS_SEARCH_URL || osExplorerSearchHost + '/search/package',
-  authUrl: process.env.OS_EXPLORER_AUTH_HOST || baseUrl,
+  baseUrl: validateUrl(baseUrl),
+  searchUrl: validateUrl(searchUrl),
+  authUrl: validateUrl(authUrl),
   snippets: {
     ga: process.env.OS_SNIPPETS_GA
   }
