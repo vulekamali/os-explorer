26d25e1571308c53d268c8ea5d01764dd2d90473
Brook Elgie <specialbrew@gmail.com>
2018-02-22 10:52:22 +0000

    Add default value for OS_COSMOPOLITAN_URL env var value
    And update README with env var values used by app.
    
    Part of openspending/openspending#1340.

--- lookenv.config.js
+++ lookenv.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   // Required settings
   OS_BASE_URL: {required: true},
-  OS_COSMOPOLITAN_URL: {required: true},
 
   // Optional settings
   OS_SNIPPETS_GA: {required: false},
@@ -11,5 +10,9 @@ module.exports = {
   OS_CONDUCTOR_URL: {required: false},
   OS_SEARCH_URL: {required: false},
   OS_EXPLORER_URL: {required: false},
-  OS_VIEWER_URL: {required: false}
+  OS_VIEWER_URL: {required: false},
+  OS_PACKAGER_URL: {required: false},
+  OS_COSMOPOLITAN_URL: {
+    default: 'https://cosmopolitan.openspending.org/?format=json'
+  }
 };
